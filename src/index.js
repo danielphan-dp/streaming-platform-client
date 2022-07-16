93824177116bcb9da703f9f6771c1faeafa6dc3b
danielphandptp <business.edwardp@gmail.com>
2022-07-16 07:44:04 -0400

    wired up the auth states with the redux store

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const store = createStore(reducers);
 const container = document.querySelector('#root');
 const root = ReactDOM.createRoot(container);
 root.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>
 );
