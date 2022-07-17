4a2e01ec5e92dbacd7699e9040caf5a3c2b972fb
danielphandptp <business.edwardp@gmail.com>
2022-07-17 13:58:59 -0400

    refactored modal buttons to separate components

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import history from '../history';
 
 const Modal = (props) => {
   return ReactDOM.createPortal(
-    <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">Modal</div>
+    <div
+      className="ui dimmer modals visible active"
+      onClick={() => history.push('/')}
+    >
+      <div
+        className="ui standard modal visible active"
+        onClick={(event) => event.stopPropagation()}
+      >
+        <div className="header">{props.title}</div>
+        <div className="content">{props.content}</div>
+        <div className="actions">{props.actions}</div>
+      </div>
     </div>,
     document.querySelector('#modal')
   );
